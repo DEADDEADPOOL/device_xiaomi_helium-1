d950a5cad529b9b7546abe4391a895054751f6c0
Rohan Hasabe <43720061+Hasaber8@users.noreply.github.com>
2018-11-10 15:53:39 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -57,6 +57,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.helium.rc
 
+# Camera
+PRODUCT_PACKAGES += \
+    Gcam
+
 # Wifi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
